ebffe0bb24d4c8b353353c9b57bae1367bb926b3
Taylor Chiles <chilestaylor@gmail.com>
2022-02-24 21:01:18 +0000

    new nav bar

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,11 @@ document.addEventListener(
   },
   false
 );
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
